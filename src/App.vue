936cc71572585f09bf9d0ab51190998d3fade440
juanca02 <jjcami2000@gmail.com>
2019-09-16 20:56:29 -0500

    .

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    
+    <Head></Head>
+    <Footer></Footer>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/Head.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Head,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
